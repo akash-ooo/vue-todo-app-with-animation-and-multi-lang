487746a53bc8442dad96987b9a37c8a91b2b65aa
Akash <mah.akash.ooo@gmail.com>
2024-03-27 08:24:37 +0600

    build: build for github page preview

--- src/locals/home/bn.js
+++
@@ -1,9 +0,0 @@
-export const bn = {
-    bn: {
-        title: 'আমার টুডু অ্যাাপ',
-        taskPlaceHolder: 'নতুন টুডু যোগ করুন',
-        pending: 'বাকি কাজ',
-        complete: 'শেষ কাজ',
-        completeBtn: 'শেষ'
-    }
-}
--- src/locals/home/en.js
+++
@@ -1,9 +0,0 @@
-export let en = {
-    en: {
-        title: 'Todo (Vue, TypScript, I18n & Vuetify)',
-        taskPlaceHolder: 'Todo (Vue, TypScript, I18n & Vuetify)',
-        pending: 'PENDING',
-        complete: 'COMPLETED',
-        completeBtn: 'COMPLETED'
-    }
-}
